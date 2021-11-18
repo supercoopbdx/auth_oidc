e19b0ead9558bcccdd29fadb32d119232cd7575e
Éloi Rivard <azmeuk@gmail.com>
2021-11-18 15:23:34 +0100

    black

--- models/res_users.py
+++ models/res_users.py
@@ -6,20 +6,20 @@ from odoo import models, api
 
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
     @api.model
     def _auth_oauth_validate(self, provider, access_token):
-        """ return the validation data corresponding to the access token """
-        oauth_provider = self.env['auth.oauth.provider'].browse(provider)
-        if oauth_provider.flow == 'id_token':
+        """return the validation data corresponding to the access token"""
+        oauth_provider = self.env["auth.oauth.provider"].browse(provider)
+        if oauth_provider.flow == "id_token":
             return oauth_provider._parse_id_token(access_token)
         else:
             return super(ResUsers, self)._auth_oauth_validate()
-    
+
     @api.model
     def auth_oauth(self, provider, params):
-        id_token = params.get('id_token')
+        id_token = params.get("id_token")
         if id_token:
-            params['access_token'] = id_token
+            params["access_token"] = id_token
         return super(ResUsers, self).auth_oauth(provider, params)
